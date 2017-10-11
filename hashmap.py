03c2a999c5ed08676725c2e3e137feea38f7fbd6
Saurabh <saurabh.dhupar@gmail.com>
2017-10-10 18:59:46 -0700

    Create hashmap.py

---
+++ hashmap.py
@@ -0,0 +1,29 @@
+'''
+Outline Oct - 10
+ - Big o Complexity
+     - Time and Space
+'''
+
+
+'''
+Big o Time Complexity 
+
+http://bigocheatsheet.com/
+https://docs.google.com/presentation/d/1Bs8skzUwgkfA30FbejcsWy2DCnD1AkIHNDyQ0sUFpRU/edit#slide=id.g1f05b8466d_0_49
+Todo : Srikant to review - Recussion Time Complexity Proof and with memonization how it coverts to linear time | Also go through slides again
+
+'''
+
+'''
+ - Use case
+     - Faster Searching
+     - Pre-Computated
+     - Key and Value can be same
+ - Concepy of HashCode
+ - Hashcode for different types
+ - Searching comparison with Arrays
+ - Effect of Choosing a bad hash function
+ - Implement Own HashMap
+ - Look at Python HashMap Implmentation
+
+'''
